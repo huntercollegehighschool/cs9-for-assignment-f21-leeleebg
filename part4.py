151371edce7dacaa277ce656ebb225ea8dbac95a
Alison Baden-Glicksman <94643819+leeleebg@users.noreply.github.com>
2022-01-05 22:21:55 +0000

    finished part 4

--- part4.py
+++ part4.py
@@ -40,3 +40,15 @@ fizz
 """
 
 # write your code here 
+
+num = int(input("Enter a number: "))
+
+for i in range(1, num+1):
+  if i%3 == 0 and i%5 != 0:
+    print("fizz")
+  if i%5 == 0 and i%3 != 0:
+    print("buzz")
+  if i%3 != 0 and i%5 != 0:
+    print(i)
+  if i%3 == 0 and i%5 == 0:
+    print("fizzbuzz")
